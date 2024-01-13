da14a0a4b11aded6a06e606aacf2f48798df9e20
befragment <109626726+befragment@users.noreply.github.com>
2024-01-13 05:04:56 +0300

    Create 0705a.cpp

---
+++ prj.codeforces/0705a.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << "I hate";
+    for (int i = 1; i < n; i++) {
+        if (i % 2 == 0) {
+            std::cout << " that I hate";
+        } else {
+            std::cout << " that I love";
+        }
+    }
+    std::cout << " it" << std::endl;
+}
