8596a32e1e4970bcfec4e3d7d65f0744547d52fe
befragment <109626726+befragment@users.noreply.github.com>
2024-01-13 08:15:23 +0300

    Update main.cpp

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -2,7 +2,7 @@
 #include "creating.hpp"
 
 int main() {
-    std::string s = static_cast<std::string>(PRJROOT) + "/kk.hpp"; // my .hpp file with declarations
+    std::string s = static_cast<std::string>(PRJROOT) + "/kk.cpp"; // my .cpp file with declarations
     TestAPICreator::create_test_api_files(s);
     return 0;
 }
